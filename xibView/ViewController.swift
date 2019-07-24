c950795d5e7614b7289fe5c0dca6bc8d4f9f2a73
Edeviatov <edeviatov@htc-cs.ru>
2019-07-24 13:09:42 +0400

    Initial Commit

---
+++ xibView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xibView
+//
+//  Created by Egor Devyatov on 24.07.2019.
+//  Copyright © 2019 Egor Devyatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
